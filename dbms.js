1dc6fb8ee294e37f4e353295be03872f2e70a568
Vishwas02mehta <mehtavansh403@gmail.com>
2025-04-16 21:03:39 +0530

    dbms.js

--- dbms.js
+++ dbms.js
@@ -1,11 +1,11 @@
-// alert("Vishwas and Kanishika welcomes you to one of the advanced auction systems of the entire world");
+alert("welcomes you to one of the advanced auction systems of the entire world");
 
-// let age = prompt("Enter your age");
+let age = prompt("Enter your age");
 
-// if (age >= 18) {
-//     console.log("A great welcome ");
+if (age >= 18) {
+    console.log("A great welcome ");
     
-// } else {
-//     alert("Bhag ja yha se! You are not allowed to access this site.");
-//     document.body.innerHTML = "<h1 style='color:red; text-align:center;'>Access Denied</h1>";
-// }
+} else {
+    alert("Bhag ja yha se! You are not allowed to access this site.");
+    document.body.innerHTML = "<h1 style='color:red; text-align:center;'>Access Denied</h1>";
+}
